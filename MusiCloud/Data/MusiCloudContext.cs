a80e3d1c8bed56bea79ced11faf996c29c34ce2a
kyle-ness <kylecness@gmail.com>
2020-12-12 17:12:12 +0200

    Created migration

---
+++ MusiCloud/Data/MusiCloudContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusiCloud.Models;
+
+namespace MusiCloud.Data
+{
+    public class MusiCloudContext : DbContext
+    {
+        public MusiCloudContext (DbContextOptions<MusiCloudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MusiCloud.Models.User> User { get; set; }
+    }
+}
---
+++ MusiCloud/Migrations/20201212150710_Init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusiCloud.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: false),
+                    DisplayName = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ConfirmPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
