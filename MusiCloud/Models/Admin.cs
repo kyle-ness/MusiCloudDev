864d1a2f991bf58e94c294ca648d3d920f7ad818
Idan Lazar <idanlazar8241@gmail.com>
2020-12-14 22:41:03 +0200

    Adding attributes to Admin class

--- MusiCloud/Models/Admin.cs
+++ MusiCloud/Models/Admin.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace MusiCloud.Models
 {
@@ -10,5 +11,13 @@ namespace MusiCloud.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
