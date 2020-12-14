5390db5c4e20ab6b9f05dd9a9c16f986a413cbc4
Idan Lazar <idanlazar8241@gmail.com>
2020-12-14 16:39:20 +0200

    Adding Concert and Admin Model

---
+++ MusiCloud/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusiCloud.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+
+    }
+}
