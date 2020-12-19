ed98830f0b55ee2dd5dbd5421e2ae7d009936ffd
Idan Lazar <idanlazar8241@gmail.com>
2020-12-19 22:22:04 +0200

    Compare models like seachBranch

--- MusiCloud/Models/Song.cs
+++ MusiCloud/Models/Song.cs
@@ -14,12 +14,12 @@ namespace MusiCloud.Models
         public string Name { get; set; }
 
         public int CounterPlayed { get; set; }
-
+       
         public string LinkToPlay { get; set; }
 
         // FK from album table
         [ForeignKey("Album")]
-        public string AlbumId { get; set; }
+        public int AlbumId { get; set; }
         public virtual Album Album { get; set; }
     }
 }
