e80e7d47c4dc100c0b901ff9f422b34ff91f6f9f
kyle-ness <kylecness@gmail.com>
2020-12-19 19:37:58 +0200

    Created playlist controller and database representation

---
+++ MusiCloud/wwwroot/js/Playlist.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "Playlist/update.html",
+        context: document.body,
+        success: function () {
+            alert("done");
+        }
+    });
