ca740e008d5a8efda60142c453b6f192308c2b0b
Idan Lazar <idanlazar8241@gmail.com>
2020-12-19 22:23:52 +0200

    compaire Usings like searchbranch

--- MusiCloud/Models/Song.cs
+++ MusiCloud/Models/Song.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Syusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MusiCloud.Models
 {
