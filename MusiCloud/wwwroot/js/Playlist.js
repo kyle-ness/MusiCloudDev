1ab80f53749c385468092371e21b29ad938687ea
kyle-ness <kylecness@gmail.com>
2020-12-20 23:11:51 +0200

    refactored the location of javascript to its file

--- MusiCloud/wwwroot/js/Playlist.js
+++ MusiCloud/wwwroot/js/Playlist.js
@@ -11,8 +11,8 @@ function LoadPlaylists(res) {
         content = '';
         res.playlists.forEach(x => {
             content +=
-                '<div class="mix col-lg-3 col-md-4 col-sm-6 movies">' +
-                '<div class="playlist-item" >' +
+                '<div id="wrapper" class="mix col-lg-3 col-md-4 col-sm-6 movies">' +
+                '<div id="playlist-item" class="playlist-item" >' +
                 '<img src="/img/playlist/'+ x.imgUrl +'.jpg" alt="">' +
                 '<h5>' + x.name + '</h5>' +
                 '</div>' +
@@ -34,4 +34,52 @@ function fetchPlaylists() {
     });
 }
 
+$("#CreatePlaylist").click(function () {
+    CreatePlaylist();
+})
 
+$('#PlaylistName').keypress(function (e) {
+    if (e.which == 13) {
+        $('#CreatePlaylist').click();
+    }
+});
+
+function CreatePlaylist() {
+    var playlist_data = {
+        'Name': $("#PlaylistName").val()
+    };
+    $("#PlaylistName").attr("placeholder", "Create more playlists").val("")
+    $.ajax({
+        url: '/Playlists/CreatePlaylistAjax',
+        data: playlist_data,
+        success: function (res) {
+            CreatePlaylistResult(res);
+        }
+    });
+}
+
+function CreatePlaylistResult(res) {
+    fetchPlaylists();
+}
+
+function FilterPlaylists() {
+    var input, filter, playlist_wrapper, playlist_items, h5, i, j, txtValue;
+    input = document.getElementById("PlaylistFilter");
+    filter = input.value.toUpperCase();
+    playlist_wrapper = document.getElementsByClassName("mix col-lg-3 col-md-4 col-sm-6 movies");
+
+    for (i = 0; i < playlist_wrapper.length; i++) {
+
+        playlist_items = playlist_wrapper[i].getElementsByTagName("div");
+        for (j = 0; j < playlist_items.length; j++) {
+            h5 = playlist_items[j].getElementsByTagName("h5")[0];
+            txtValue = h5.textContent || h5.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                playlist_wrapper[i].style.display = "";
+            } else {
+                playlist_wrapper[i].style.display = "none";
+            }
+        }
+
+    }
+}
